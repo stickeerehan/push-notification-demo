1c6a8b3b378e2c4a737ef513e3036fccce994e8b
Rehan Aslam <rehan.aslam@stickee.co.uk>
2018-08-23 15:40:08 +0100

    Commit

---
+++ config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+    gcmAPIKey: 'AAAAN25aXtw:APA91bE3JP5vvVr88PDYeHJdYC4OyKEwuQw3-y0Z8qk8ugjGzg7WMpoJMYndY1YRVV4z9HBiFSu1kR0R3SwpWS0ztFkEpSwUrmk7RIouUn_pfeCpJ1FYKXer3dpOPKMI7INdagbY_OWP'
+};
